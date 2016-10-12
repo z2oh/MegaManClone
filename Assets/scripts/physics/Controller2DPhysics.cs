85e3eaac61d2f4069c4de43ea264739b555a5b8e
Max Forst <Max Forst>
2016-10-12 00:06:56 -0400

    Added: Controller2DPhysics. This script does four things: first it allows you to use physics terms such as velocity and acceleration rather than the Controller2D move command. Second it allows scripts to segregate their velocity inputs i.e. a moving platform can move the player up and down with out interfering with the player's ability to jump. Third it allows a script to lock the player into a specific velocity for a short period of time i.e. basis for a knock back or stun script. Finally it establishes a motion hierarchy: collisions overrides motion locks overrides physics.
    Movement_Experimental is an experimental jumping script that currently allows variable jump heights by manipulating the gravity acceleration based on a timer. It is a start but the differences in acceleration are visibly noticeable and makes for odd jerky jumps. I will experiment with the script, possibly by modifying both the initial velocity and the acceleration to create a smoother variable jump.
    
    Currently the code is uncommented. That will be fixed at a later time

---
+++ Assets/scripts/physics/Controller2DPhysics.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Controller2D))]
+public class Controller2DPhysics : MonoBehaviour {
+
+	// Use this for initialization
+	[HideInInspector]
+	public Vector2 velocity;
+
+	Dictionary<string, physics_data> physics;
+
+	Controller2D controller;
+	Vector2 min, max;
+	float lock_duration;
+
+	// Use this for initialization
+	void Start () {
+		lock_duration = 0;
+
+		controller = GetComponent<Controller2D> ();
+		physics = new Dictionary<string, physics_data> ();
+		physics ["DEFAULT"] = new physics_data ();
+		physics ["DEFAULT"].Reset ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float delta = Time.deltaTime;
+
+		//calculate net values
+		Vector2 net_velocity = new Vector2 (0, 0);
+		Vector2 net_acceleration = new Vector2 (0, 0);
+		List<string> keys = new List<string> (physics.Keys);
+		foreach (string key in keys) {
+			physics_data phys = physics [key];
+			if (phys.dying == true)
+				phys.time_remaining -= delta;
+			if (phys.dying == true && phys.time_remaining < 0)
+				physics.Remove (key);
+			else {
+				phys.velocity += delta * phys.acceleration - phys.velocity * phys.damping;
+				phys.acceleration = new Vector2 (0, 0);
+				if (lock_duration > 0) {
+					if (phys.velocity.x < min.x)
+						phys.velocity = new Vector2 (min.x, phys.velocity.y);
+					if (phys.velocity.x > max.x)
+						phys.velocity = new Vector2 (max.x, phys.velocity.y);
+					if (phys.velocity.y < min.y)
+						phys.velocity = new Vector2 (phys.velocity.x, min.y);
+					if (phys.velocity.y > max.y)
+						phys.velocity = new Vector2 (phys.velocity.x, max.y);
+
+					if (controller.collisions.left && phys.velocity.x < 0)
+						phys.velocity = new Vector2 (0, phys.velocity.y);
+					if(controller.collisions.right && phys.velocity.x > 0)
+						phys.velocity = new Vector2 (0, phys.velocity.y);
+					if (controller.collisions.below && phys.velocity.y < 0)
+						phys.velocity = new Vector2 (phys.velocity.x, 0);
+					if(controller.collisions.above && phys.velocity.y > 0)
+						phys.velocity = new Vector2 (phys.velocity.x, 0);
+				}
+
+				physics [key] = phys;
+				net_velocity += phys.velocity;
+			}
+		}
+		if(lock_duration > 0)
+			lock_duration -= delta;
+
+		controller.Move (delta * new Vector3(net_velocity.x, net_velocity.y, 0));
+	}
+
+	public void Force_Velocity(Vector2 v1, Vector2 v2, float time){
+		min = new Vector2 ((v1.x > v2.x) ? v2.x : v1.x, (v1.y > v2.y) ? v2.y : v1.y);
+		max = new Vector2 ((v1.x > v2.x) ? v1.x : v2.x, (v1.y > v2.y) ? v1.y : v2.y);
+		lock_duration = time;
+	}
+
+	public void Set_Velocity (Vector2 value, string name = "DEFAULT"){
+		if (!physics.ContainsKey (name)) {
+			physics [name] = new physics_data ();
+			physics [name].Reset ();
+		}
+		physics_data phys = physics [name];
+		phys.velocity = value;
+		physics [name] = phys;
+	}
+
+	public void Accelerate (Vector2 value, string name = "DEFAULT"){
+		if (!physics.ContainsKey (name)) {
+			physics [name] = new physics_data ();
+			physics [name].Reset ();
+		}
+		physics_data phys = physics [name];
+		phys.acceleration += value;
+		physics [name] = phys;
+	}
+
+	public void Set_Damping (float value, string name = "DEFAULT"){
+		if (!physics.ContainsKey (name)) {
+			physics [name] = new physics_data ();
+			physics [name].Reset ();
+		}
+		physics_data phys = physics [name]; 
+		phys.damping = value;
+	}
+
+	public void Kill (float time, string name){
+		if (name == "DEFAULT" || !physics.ContainsKey (name))
+			return;
+		physics_data phys = physics [name];
+		phys.decay_time = time;
+		phys.time_remaining = time;
+		phys.dying = true;
+		physics [name] = phys;
+	}
+
+	public physics_data Get_Physics_Data(string name = "DEFAULT"){
+		if(!physics.ContainsKey(name)){
+			physics [name] = new physics_data ();
+			physics [name].Reset ();
+		}
+		return physics [name];
+	}
+	public struct physics_data
+	{
+		public Vector2 velocity;
+		public Vector2 acceleration;
+		public float damping;
+		public float decay_time;
+		public float time_remaining;
+		public bool dying;
+
+		public void Reset(){
+			velocity = new Vector2 (0, 0);
+			acceleration= new Vector2 (0, 0);
+			damping = 0f;
+			decay_time = -1;
+			time_remaining = -1;
+			dying = false;
+		}
+	}
+}
---
+++ Assets/scripts/player/Movement_Experimental.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Controller2DPhysics))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class Movement_Experimental : MonoBehaviour {
+
+	public float min_jump_height;
+	public float max_jump_height;
+	public float max_standing_jump_duration;
+	//public float long_jump_ratio;
+
+	public float horz_movement_speed;
+	public float ground_acceleration;
+	public float air_acceleration;
+
+	public float vert_velocity_cap;
+
+	float horz_smoothing;
+	public float horz_smoothing_time;
+
+	float max_acceleration, min_acceleration, jump_time, initial_jump_velocity;
+	Vector2 up, right;
+
+	bool jump_released;
+
+	SpriteRenderer sprite;
+	Controller2DPhysics physics;
+	Controller2D controller;
+	// Use this for initialization
+	void Start () {
+		max_acceleration = 2 * max_jump_height / (max_standing_jump_duration * max_standing_jump_duration);
+		initial_jump_velocity = 2 * max_jump_height / max_standing_jump_duration;
+		min_acceleration = initial_jump_velocity * initial_jump_velocity / (2 * min_jump_height);
+
+		jump_time = max_standing_jump_duration;
+		up = Vector2.up;
+		right = Vector2.right;
+
+		horz_smoothing = 0f;
+
+		sprite = GetComponent<SpriteRenderer> ();
+		physics = GetComponent<Controller2DPhysics> ();
+		//physics.Set_Velocity (new Vector2 (0, 0), "Player_Input");
+		controller = GetComponent<Controller2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float delta = Time.deltaTime;
+
+		physics.Set_Velocity (Calculate_Horz_Velocity (delta) + Calculate_Vert_Velocity (delta), "Player_Input");
+
+	}
+
+	Vector2 Calculate_Vert_Velocity(float time){
+		Vector2 velocity = physics.Get_Physics_Data ("Player_Input").velocity;
+		float speed = velocity.x * up.x + velocity.y * up.y;
+		if (controller.collisions.below) {
+			jump_released = false;
+			jump_time = max_standing_jump_duration;
+
+			if (Input.GetButton ("Jump")) {
+				speed = initial_jump_velocity;
+			}
+		} else {
+			if (speed > 0) {
+				if (!Input.GetButton ("Jump"))
+					jump_released = true;
+				if (jump_released) {
+					speed -= time * min_acceleration;
+				} else {
+					speed -= time * max_acceleration;
+					jump_time -= time;
+				}
+			} else {
+				if (jump_time > 0) {
+					speed -= time * max_acceleration;
+					jump_time -= time;
+				} else
+					speed -= time * min_acceleration;
+			}
+		}
+		if (speed < -vert_velocity_cap)
+			speed = -vert_velocity_cap;
+		return up * speed;
+	}
+	Vector2 Calculate_Horz_Velocity(float time){
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		Vector2 velocity = physics.Get_Physics_Data ("Player_Input").velocity;
+		float speed = velocity.x * right.x + velocity.y * right.y;
+		if(input.x > 0)
+		{
+			sprite.flipX = true;
+		}
+		else if(input.x < 0)
+		{
+			sprite.flipX = false;
+		}
+		speed = Mathf.SmoothDamp(speed, input.x * horz_movement_speed, ref horz_smoothing, (controller.collisions.below) ? ground_acceleration : air_acceleration);
+		return right * speed;
+	}
+}
