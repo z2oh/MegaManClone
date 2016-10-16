1c7d7ea9883b490fdd0b4b1ddfa7e1a0a46f14a6
Max Forst <Max Forst>
2016-10-16 12:05:04 -0400

    Various bug fixes and code improvements to Movement_Experimental. Added comments to Movement_Experimental and Controller2DPhysics

--- Assets/scripts/player/Movement_Experimental.cs
+++ Assets/scripts/player/Movement_Experimental.cs
@@ -16,11 +16,11 @@ public class Movement_Experimental : MonoBehaviour {
 
 	public float decay_time, transition_time;
 
-	float jump_transition;
 	public float horz_smoothing_time;
 
 	float max_acceleration, min_acceleration, jump_time, initial_jump_velocity;
 	float min_initial_jump_velocity;
+	//these are basis vectors. Changing them will change the direction of the the entire physics script
 	Vector2 up, right;
 
 	bool jump_released;
@@ -28,12 +28,13 @@ public class Movement_Experimental : MonoBehaviour {
 	SpriteRenderer sprite;
 	Controller2DPhysics physics;
 	Controller2D controller;
-	// Use this for initialization
+
+
 	void Start () {
 		max_acceleration = 2 * max_jump_height / (max_standing_jump_duration * max_standing_jump_duration);
-		//initial_jump_velocity = 2 * max_jump_height / max_standing_jump_duration;
+
 		initial_jump_velocity = Mathf.Sqrt(2 * max_acceleration * max_jump_height);
-		//min_acceleration = initial_jump_velocity * initial_jump_velocity / (2 * min_jump_height);
+
 		min_initial_jump_velocity = Mathf.Sqrt(2 * max_acceleration * min_jump_height);
 
 
@@ -43,95 +44,63 @@ public class Movement_Experimental : MonoBehaviour {
 
 		sprite = GetComponent<SpriteRenderer> ();
 		physics = GetComponent<Controller2DPhysics> ();
-		//physics.Set_Velocity (new Vector2 (0, 0), "Player_Input");
+
 		controller = GetComponent<Controller2D> ();
 	}
 	
-	// Update is called once per frame
+	//this should really be a fixed update but it might be ok if it is an update instead
 	void FixedUpdate () {
+		//calcualte the change in time
 		float delta = Time.deltaTime;
-
+		//update the player velocity
 		physics.Set_Velocity (Calculate_Horz_Velocity (delta) + Calculate_Vert_Velocity (delta), "Player_Input");
 
 	}
-	/*
-	Vector2 Calculate_Vert_Velocity(float time){
-		Vector2 velocity = physics.Get_Physics_Data ("Player_Input").velocity;
-		float speed = velocity.x * up.x + velocity.y * up.y;
-		if (controller.collisions.below) {
-			jump_released = false;
-			jump_time = max_standing_jump_duration;
-
-			if (Input.GetButton ("Jump")) {
-				speed = initial_jump_velocity;
-			}
-		} else {
-			if (speed > 0) {
-				if (!Input.GetButton ("Jump"))
-					jump_released = true;
-				if (jump_released) {
-					speed -= time * min_acceleration;
-				} else {
-					speed -= time * max_acceleration;
-					jump_time -= time;
-				}
-			} else {
-				if (jump_time > 0) {
-					speed -= time * max_acceleration;
-					jump_time -= time;
-				} else
-					speed -= time * min_acceleration;
-			}
-		}
-		if (speed < -vert_velocity_cap)
-			speed = -vert_velocity_cap;
-		return up * speed;
-	}*/
 
 	Vector2 Calculate_Vert_Velocity(float time){
+		//get the extant physics data
 		Vector2 velocity = physics.Get_Physics_Data("Player_Input").velocity;
+		//filter by direction using a dot product
+		//since the basis vectors are orthonormal this will give the projection of velocity onto
+		//up
 		float base_speed = velocity.x * up.x + velocity.y * up.y;
 		velocity = physics.Get_Physics_Data("Max_Jump").velocity;
 		float add_speed = velocity.x * up.x + velocity.y * up.y;
+		//check to see if on the ground
+		//if we like this script I will rewrite the script to account for a rotated basis
+		//i.e. a change in gravity direction
 		if (controller.collisions.below) {
+			//reset this bool
 			jump_released = false;
-
+			//jump if the player presses jump
 			if (Input.GetButton ("Jump")) {
-				jump_time = decay_time;
+				//jump_time = decay_time;
 				base_speed = min_initial_jump_velocity;
 				physics.Set_Velocity (up * (initial_jump_velocity - base_speed), "Max_Jump");
+			} else {//don't jump if they didn't press jump
+				physics.Set_Velocity (up * 0, "Max_Jump");
+				base_speed = 0;
 			}
-		} else {
-			if (base_speed + add_speed > 0) {
+		} else {//if in the air
+			if (base_speed + add_speed > 0) {// if the player is moving up
 				if (!Input.GetButton ("Jump")) {
 					jump_released = true;
-					jump_transition = transition_time;
-				}
+				}//if the player releases jump cut the jump short
 				if (jump_released){
-					jump_transition -= time;
-					if (jump_time < 0)
-						jump_time = 0;
-					physics.Set_Velocity ((jump_transition / transition_time) * up * (initial_jump_velocity - min_initial_jump_velocity), "Max_Jump");
+					physics.Kill(transition_time, "Max_Jump");
 				}
-				else {
-					physics.Set_Velocity (up * (initial_jump_velocity - min_initial_jump_velocity), "Max_Jump");
-					//jump_time -= time;
-				}
-			} else {
-				jump_time -= time;
-				if (jump_time < 0)
-					jump_time = 0;
-				physics.Set_Velocity ((jump_time / decay_time) * up * (initial_jump_velocity - min_initial_jump_velocity), "Max_Jump");
+			} else {//if the player is falling
+				physics.Kill( decay_time, "Max_Jump"); // clear out any remaining jump data
 			}
+			//accelerate downward
 			base_speed -= time * max_acceleration;
 		}
+		//return the new data
 		return up * base_speed;
 	}
 
 	Vector2 Calculate_Horz_Velocity(float time){
 		Vector2 input = Input.GetAxisRaw("Horizontal") * right;
-		//Vector2 velocity = physics.Get_Physics_Data ("Player_Input").velocity;
-		//float speed = velocity.x * right.x + velocity.y * right.y;
 		if(input.x > 0)
 		{
 			sprite.flipX = true;
@@ -140,7 +109,6 @@ public class Movement_Experimental : MonoBehaviour {
 		{
 			sprite.flipX = false;
 		}
-		//speed = Mathf.SmoothDamp(speed, input.x * horz_movement_speed, ref horz_smoothing, (controller.collisions.below) ? ground_acceleration : air_acceleration);
 		float speed = horz_movement_speed;
 		return input * speed;
 	}
