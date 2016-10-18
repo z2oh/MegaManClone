58d09eb65a3bdf5fc2cd3558db3ddb90bd20d754
z2oh <jadaytime@gmail.com>
2016-10-18 18:42:14 -0400

    added health script, implements #9, #10

---
+++ Assets/scripts/player/Gun.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    public Transform bullet;
+
+    void Shoot()
+    {
+        Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+}
