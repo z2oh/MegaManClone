cbbb10eb29bd5913c984c630edcec7d31c36d132
jeff knoll <jeff knoll>
2016-10-23 16:08:33 -0400

    added scripts

---
+++ Assets/scripts/player/MoveScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour
+{
+
+    public Vector2 speed = new Vector2(10, 10);
+    public Vector2 direction = new Vector2(-1, 0);
+    private Vector2 movement;
+    private Rigidbody2D rigidbodyComponent;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(speed.x * direction.x, speed.y * direction.y);
+    }
+    void FixedUpdate()
+    {
+        if (rigidbodyComponent == null)
+        {
+            rigidbodyComponent.velocity = movement;
+
+        }
+    }
+}
+
