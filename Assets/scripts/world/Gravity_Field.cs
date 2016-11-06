b86bc4b0b0bddd703ba4cb6737dc321df0cf4a0c
Max Forst <Max Forst>
2016-11-06 12:01:38 -0500

    Added some alternative jump scripts

--- Assets/scripts/world/Gravity_Field.cs
+++ Assets/scripts/world/Gravity_Field.cs
@@ -5,9 +5,11 @@ public class Gravity_Field : MonoBehaviour {
 
     public int direction;
     public bool mirrored;
+    public string target;
 
 	void OnTriggerEnter2D(Collider2D other)
     {
-        Gravity.Set_Gravity(direction, mirrored);
+        if(target == other.tag)
+            Gravity.Set_Gravity(direction, mirrored);
     }
 }
